fa2e583bbc1d4e5d1eb6adb75f6daedf496843d6
Ashvin Warrier <warrierar@gmail.com>
2018-09-03 21:30:29 -0400

    Implemented Person1 calc

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -3,9 +3,9 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 1
  *  returns their name and a
- *  modified string 
- *  
- *  @author Bob
+ *  modified string
+ *
+ *  @author Ashvin Warrier
  *  @version 1.1
  */
 public class Person1 {
@@ -30,16 +30,17 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	  String temp = input.substring(2);
+      temp.concat(input.substring(0,2));
+	  return temp;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
